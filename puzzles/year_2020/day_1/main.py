8cffef688ff6a6dc89682bd30ed4f8563f86ac2a
Ben Watson <fig.chutney8@gmail.com>
2021-11-12 10:14:32 +0000

    Simplify structure and add part 2 answer

--- puzzles/year_2020/day_1/main.py
+++ puzzles/year_2020/day_1/main.py
@@ -1,20 +1,40 @@
-from typing import List, Tuple
+from typing import List
 
 from .data import EXPENSE_REPORT
 
+# PART 1
 
-def get_2_values_that_sum_to_2020(values: List[int]) -> Tuple[int, int]:
+
+def multiply_2_values_that_sum_to_2020(
+    values: List[int],
+) -> int:
     for value_1 in values:
         for value_2 in values:
             if value_1 + value_2 == 2020:
-                return (value_1, value_2)
-    raise ValueError("Uh oh! None of the values sum to 2020 :'(")
+                return value_1 * value_2
+    raise ValueError("Uh oh! No two values sum to 2020 :'(")
 
 
-def main(expense_report: List[int] = EXPENSE_REPORT) -> int:
-    expense_1, expense_2 = get_2_values_that_sum_to_2020(values=expense_report)
-    return expense_1 * expense_2
+# PART 2
+
+
+def multiply_3_values_that_sum_to_2020(
+    values: List[int],
+) -> int:
+    for value_1 in values:
+        for value_2 in values:
+            for value_3 in values:
+                if value_1 + value_2 + value_3 == 2020:
+                    return value_1 * value_2 * value_3
+    raise ValueError("Uh oh! No three values sum to 2020 :'(")
 
 
 if __name__ == "__main__":
-    print(main())
+    print(
+        f"Part 1 Answer: "
+        f"{multiply_2_values_that_sum_to_2020(values=EXPENSE_REPORT)}"
+    )
+    print(
+        f"Part 2 Answer: "
+        f"{multiply_3_values_that_sum_to_2020(values=EXPENSE_REPORT)}"
+    )
--- tests/year_2020/day_1/test_main.py
+++ tests/year_2020/day_1/test_main.py
@@ -3,13 +3,17 @@ from puzzles.year_2020.day_1 import main
 from .data import DUMMY_EXPENSE_REPORT
 
 
-def test_get_2_values_that_sum_to_2020() -> None:
-    actual = main.get_2_values_that_sum_to_2020(values=DUMMY_EXPENSE_REPORT)
-    expected = (1721, 299)
+def test_multiply_2_values_that_sum_to_2020() -> None:
+    actual = main.multiply_2_values_that_sum_to_2020(
+        values=DUMMY_EXPENSE_REPORT
+    )
+    expected = 514579
     assert actual == expected
 
 
-def test_main() -> None:
-    actual = main.main(expense_report=DUMMY_EXPENSE_REPORT)
-    expected = 514579
+def test_multiply_3_values_that_sum_to_2020() -> None:
+    actual = main.multiply_3_values_that_sum_to_2020(
+        values=DUMMY_EXPENSE_REPORT
+    )
+    expected = 241861950
     assert actual == expected
