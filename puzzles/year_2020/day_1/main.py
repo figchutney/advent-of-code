379dfa3f6453ff0406019afd125e5d0e513581ca
Ben Watson <fig.chutney8@gmail.com>
2021-11-12 18:04:42 +0000

    Alter solution to avoid big O problem

--- puzzles/year_2020/day_1/main.py
+++ puzzles/year_2020/day_1/main.py
@@ -1,18 +1,55 @@
-from typing import List
+from typing import Dict, List, Tuple
 
 from .data import EXPENSE_REPORT
+
+
+def make_values_dict(values: List[int]) -> Dict[int, int]:
+    return {value: value for value in values}
+
+
+def _find_pair_that_sum_to_target(
+    values: List[int],
+    values_dict: Dict[int, int],
+    target: int,
+) -> Tuple[int, int]:
+    for value in values:
+        try:
+            return (value, values_dict[target - value])
+        except KeyError:
+            pass
+    raise ValueError(f"Uh oh! No two values sum to {target} :'(")
+
+
+def _find_triplet_that_sum_to_target(
+    values: List[int],
+    values_dict: Dict[int, int],
+    target: int,
+) -> Tuple[int, int, int]:
+    for value in values:
+        try:
+            return (value,) + _find_pair_that_sum_to_target(
+                values=values,
+                values_dict=values_dict,
+                target=target - value,
+            )
+        except (ValueError, KeyError):
+            pass
+    raise ValueError(f"Uh oh! No three values sum to {target} :'(")
+
 
 # PART 1
 
 
 def multiply_2_values_that_sum_to_2020(
     values: List[int],
+    values_dict: Dict[int, int],
 ) -> int:
-    for value_1 in values:
-        for value_2 in values:
-            if value_1 + value_2 == 2020:
-                return value_1 * value_2
-    raise ValueError("Uh oh! No two values sum to 2020 :'(")
+    value_1, value_2 = _find_pair_that_sum_to_target(
+        values=values,
+        values_dict=values_dict,
+        target=2020,
+    )
+    return value_1 * value_2
 
 
 # PART 2
@@ -20,21 +57,27 @@ def multiply_2_values_that_sum_to_2020(
 
 def multiply_3_values_that_sum_to_2020(
     values: List[int],
+    values_dict: Dict[int, int],
 ) -> int:
-    for value_1 in values:
-        for value_2 in values:
-            for value_3 in values:
-                if value_1 + value_2 + value_3 == 2020:
-                    return value_1 * value_2 * value_3
-    raise ValueError("Uh oh! No three values sum to 2020 :'(")
+    value_1, value_2, value_3 = _find_triplet_that_sum_to_target(
+        values=values,
+        values_dict=values_dict,
+        target=2020,
+    )
+    return value_1 * value_2 * value_3
 
 
 if __name__ == "__main__":
-    print(
-        f"Part 1 Answer: "
-        f"{multiply_2_values_that_sum_to_2020(values=EXPENSE_REPORT)}"
+
+    values_dict = make_values_dict(values=EXPENSE_REPORT)
+
+    part_1_answer = multiply_2_values_that_sum_to_2020(
+        values=EXPENSE_REPORT,
+        values_dict=make_values_dict(values=EXPENSE_REPORT),
     )
-    print(
-        f"Part 2 Answer: "
-        f"{multiply_3_values_that_sum_to_2020(values=EXPENSE_REPORT)}"
+    part_2_answer = multiply_3_values_that_sum_to_2020(
+        values=EXPENSE_REPORT,
+        values_dict=make_values_dict(values=EXPENSE_REPORT),
     )
+    print(f"Part 1 Answer: {part_1_answer}")
+    print(f"Part 2 Answer: {part_2_answer}")
