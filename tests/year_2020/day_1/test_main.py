473d6e3da03b3698cbf538b57f4cb1cbdcf7f876
Ben Watson <fig.chutney8@gmail.com>
2021-11-15 19:21:17 +0000

    Add tests

--- tests/year_2020/day_1/test_main.py
+++ tests/year_2020/day_1/test_main.py
@@ -1,19 +1,38 @@
 from puzzles.year_2020.day_1 import main
 
-from .data import DUMMY_EXPENSE_REPORT
+DUMMY_VALUES = {
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456,
+}
 
 
-def test_multiply_2_values_that_sum_to_2020() -> None:
-    actual = main.multiply_2_values_that_sum_to_2020(
-        values=DUMMY_EXPENSE_REPORT
-    )
+def test_solve_part_1() -> None:
+    actual = main.solve_part_1(values=DUMMY_VALUES)
     expected = 514579
     assert actual == expected
 
 
-def test_multiply_3_values_that_sum_to_2020() -> None:
-    actual = main.multiply_3_values_that_sum_to_2020(
-        values=DUMMY_EXPENSE_REPORT
-    )
+def test_solve_part_2() -> None:
+    actual = main.solve_part_2(values=DUMMY_VALUES)
     expected = 241861950
+    assert actual == expected
+
+
+def test__find_pair_that_sum_to_target() -> None:
+    actual = main._find_pair_that_sum_to_target(
+        values=DUMMY_VALUES, target=2020
+    )
+    expected = (1721, 299)
+    assert actual == expected
+
+
+def test__find_triplet_that_sum_to_target() -> None:
+    actual = main._find_triplet_that_sum_to_target(
+        values=DUMMY_VALUES, target=2020
+    )
+    expected = (675, 979, 366)
     assert actual == expected
