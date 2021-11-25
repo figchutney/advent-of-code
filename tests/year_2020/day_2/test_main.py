80592392f11fb7a959cd5b405540f7af59b62301
Ben Watson <fig.chutney8@gmail.com>
2021-11-25 11:01:56 +0000

    Add tests for day 2 part 2 and make tests neater

--- tests/year_2020/day_2/test_main.py
+++ tests/year_2020/day_2/test_main.py
@@ -1,69 +1,100 @@
 from puzzles.year_2020.day_2 import main
 
+DUMMY_PASSWORD = main.Password(
+    policy_letter="m",
+    constraint_lower=1,
+    constraint_upper=4,
+    password="mrfmmbjxr",
+)
+DUMMY_PASSWORDS = [
+    main.Password(
+        policy_letter="a",
+        constraint_lower=1,
+        constraint_upper=3,
+        password="abcde",
+    ),
+    main.Password(
+        policy_letter="b",
+        constraint_lower=1,
+        constraint_upper=3,
+        password="cdefg",
+    ),
+    main.Password(
+        policy_letter="c",
+        constraint_lower=2,
+        constraint_upper=9,
+        password="ccccccccc",
+    ),
+]
+
 
 def test__parse_password_and_policy() -> None:
 
     dummy_value = "1-4 m: mrfmmbjxr"
 
-    expected = main.Password(
-        required_letter="m",
-        letter_count_min=1,
-        letter_count_max=4,
-        password="mrfmmbjxr",
-    )
+    expected = DUMMY_PASSWORD
     actual = main._parse_password_and_policy(value=dummy_value)
 
     assert actual == expected
 
 
-def test__is_password_valid_true() -> None:
+def test__is_letter_count_within_constraints_true() -> None:
+
+    expected = True
+    actual = main._is_letter_count_within_constraints(p=DUMMY_PASSWORD)
+
+    assert actual == expected
+
+
+def test__is_letter_count_within_constraints_false() -> None:
 
     dummy_password = main.Password(
-        required_letter="m",
-        letter_count_min=1,
-        letter_count_max=4,
+        policy_letter="x",
+        constraint_lower=2,
+        constraint_upper=4,
         password="mrfmmbjxr",
     )
 
-    assert main._is_password_valid(password=dummy_password) is True
+    expected = False
+    actual = main._is_letter_count_within_constraints(p=dummy_password)
+
+    assert actual == expected
 
 
-def test__is_password_valid_false() -> None:
+def test__is_letter_in_one_constraint_position_true() -> None:
 
     dummy_password = main.Password(
-        required_letter="x",
-        letter_count_min=2,
-        letter_count_max=4,
+        policy_letter="m",
+        constraint_lower=1,
+        constraint_upper=3,
         password="mrfmmbjxr",
     )
 
-    assert main._is_password_valid(password=dummy_password) is False
+    expected = True
+    actual = main._is_letter_in_one_constraint_position(p=dummy_password)
+
+    assert actual == expected
+
+
+def test__is_letter_in_one_constraint_position_false() -> None:
+
+    expected = False
+    actual = main._is_letter_in_one_constraint_position(p=DUMMY_PASSWORD)
+
+    assert actual == expected
 
 
 def test_solve_part_1() -> None:
 
-    dummy_passwords = [
-        main.Password(
-            required_letter="a",
-            letter_count_min=1,
-            letter_count_max=3,
-            password="abcde",
-        ),
-        main.Password(
-            required_letter="b",
-            letter_count_min=1,
-            letter_count_max=3,
-            password="cdefg",
-        ),
-        main.Password(
-            required_letter="c",
-            letter_count_min=2,
-            letter_count_max=9,
-            password="ccccccccc",
-        ),
-    ]
-
     expected = 2
-    actual = main.solve_part_1(passwords=dummy_passwords)
+    actual = main.solve_part_1(passwords=DUMMY_PASSWORDS)
+
+    assert actual == expected
+
+
+def test_solve_part_2() -> None:
+
+    expected = 1
+    actual = main.solve_part_2(passwords=DUMMY_PASSWORDS)
 
     assert actual == expected
