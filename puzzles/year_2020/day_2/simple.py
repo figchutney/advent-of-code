855d41cb09ab19c7bbec76a47c33485b249d7e9e
Ben Watson <fig.chutney8@gmail.com>
2021-11-25 11:01:34 +0000

    Make day 2 simple solution neater

--- puzzles/year_2020/day_2/simple.py
+++ puzzles/year_2020/day_2/simple.py
@@ -5,12 +5,29 @@ if __name__ == "__main__":
     with open(Path(__file__).resolve().parent / "data.txt") as f:
         passwords = [line for line in f]
 
-    # PART 1
-    valid_passwords = 0
-    for string in passwords:
-        range, letter, password = string.replace(":", "").split(" ")
-        min, max = range.split("-")
-        if int(min) <= password.count(letter) <= int(max):
-            valid_passwords += 1
+    valid_passwords_part_1 = 0
+    valid_passwords_part_2 = 0
 
-    print(valid_passwords)
+    for string in passwords:
+
+        range, letter, password = string.replace(":", "").split(" ")
+        value_1, value_2 = range.split("-")
+
+        # PART 1
+        if int(value_1) <= password.count(letter) <= int(value_2):
+            valid_passwords_part_1 += 1
+
+        # PART 2
+        if (
+            password[int(value_1) - 1] == letter
+            and password[int(value_2) - 1] != letter
+        ) or (
+            password[int(value_1) - 1] != letter
+            and password[int(value_2) - 1] == letter
+        ):
+            valid_passwords_part_2 += 1
+
+    # PART 1
+    print(valid_passwords_part_1)
+    # PART 2
+    print(valid_passwords_part_2)
