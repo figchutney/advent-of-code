cfc6cbf9ed8ea4cac294091c39eced56ae13b068
Ben Watson <fig.chutney8@gmail.com>
2021-12-01 19:50:28 +0000

    Add Y2021;DAY1 solution

---
+++ puzzles/year_2021/day_1/main.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+def get_depths() -> list[int]:
+    with open(Path(__file__).resolve().parent / "data.txt") as f:
+        return [int(depth) for depth in f.readlines()]
+
+
+def count_depth_increases(depths: list[int]) -> int:
+    return sum(b > a for a, b in zip(depths, depths[1:]))
+
+
+def get_window_sums(depths: list[int]) -> list[int]:
+    return [a + b + c for a, b, c in zip(depths, depths[1:], depths[2:])]
+
+
+if __name__ == "__main__":
+
+    depths = get_depths()
+
+    # PART 1
+    print(count_depth_increases(depths=depths))
+
+    # PART 2
+    print(count_depth_increases(depths=(get_window_sums(depths=depths))))
